ad50f54245a2bc10ce6a36ef2bde90344f9fa942
Guillem Fernandez Lopez <guillem.fernandez.lopez@enti.cat>
2025-02-22 22:09:13 +0100

    Vida inacabada

---
+++ Assets/code/player/hp.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class hp : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealthPoints = 6;
+    [SerializeField]
+    private int healthPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthPoints = maxHealthPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            healthPoints -= Damage();
+        }
+
+        if (healthPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    private int Damage()
+    {
+        int ataque = 1;
+
+        return ataque;
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public int getHP()
+    {
+        return healthPoints;
+    }
+
+    public void setHP(int perderVida)
+    {
+        healthPoints -= perderVida;
+    }
+}
