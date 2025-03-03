c16347432eb7d746fb75535a6226c267c49c7408
Guillem Fernandez Lopez <guillem.fernandez.lopez@enti.cat>
2025-03-03 13:50:42 +0100

    Canbios en mi escena

--- Assets/code/player/GroundDetector.cs
+++ Assets/code/player/GroundDetector.cs
@@ -44,10 +44,7 @@ public class GroundDetector : MonoBehaviour
             if(hit.collider != null)
             {
                 count++;
-<<<<<<< HEAD
-               // Debug.Log(hit.collider.name);
-=======
->>>>>>> origin/feature/CameraLevel
+
 
             }
 
