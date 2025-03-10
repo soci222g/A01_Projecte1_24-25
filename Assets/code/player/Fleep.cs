6ca1e6b4ba8ffc50504b8b628b2cf2e9669596ff
soci222g <116338784+soci222g@users.noreply.github.com>
2025-03-10 19:50:50 +0100

    timer al fleep

--- Assets/code/player/Fleep.cs
+++ Assets/code/player/Fleep.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Fleep : MonoBehaviour
 {
@@ -10,15 +11,20 @@ public class Fleep : MonoBehaviour
     [SerializeField]
     private bool fleep;
     private bool fleepControler = true;
-
     [SerializeField]
+    private bool flying;
+   
     private int fleepTime = 1;
-
+    [SerializeField]
     private float curentfleepTime;
     // Update is called once per frame
 
+
+
+
     void Start()
     {
+        flying = false;
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
@@ -26,8 +32,17 @@ public class Fleep : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(curentfleepTime <= 0)
         ActivateGravity();
+        else
+            curentfleepTime -= Time.deltaTime;
 
+
+        if(flying && GetComponent<GroundDetector>().GetGroundDetect())
+        {
+            curentfleepTime = fleepTime;
+            flying = false;
+        }
     }
 
 
@@ -47,15 +62,13 @@ public class Fleep : MonoBehaviour
         {
             rb.gravityScale *= -1;
             sr.flipY = !sr.flipY;
+            flying = true;
         }
-    }
-
-    public void SetCurrentFleepTimer() {
-
-        curentfleepTime = fleepTime;
 
 
     }
+
+
 
     public bool GetFleepControler()
     {
