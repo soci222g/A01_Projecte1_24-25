ac0708dd4ccbcde5927c13ee9871c4b3f39b87c6
cachau <joel.cruz@enti.cat>
2025-03-17 13:58:02 +0100

    atack sin acabar

--- Assets/code/enemis/kogmaw/kogmas_atack.cs
+++ Assets/code/enemis/kogmaw/kogmas_atack.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class kogmas_atack : MonoBehaviour
 {
+
+    [SerializeField] BoxCollider2D coll;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        coll.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void kogmaw_atack_hitbox_enabled()
+    {
+        coll.enabled = true;
+        Debug.Log("atacking");
+    }
+
+
+    void kogmaw_atack_hitbox_disable()
+    {
+        coll.enabled = false;
+        Debug.Log("finished atack");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("hit");
     }
 }
