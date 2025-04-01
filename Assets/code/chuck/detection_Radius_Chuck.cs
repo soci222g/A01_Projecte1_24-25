3e53cf386ed0c636ab9559c0f1af5802ebac2846
Guillem Fernandez Lopez <guillem.fernandez.lopez@enti.cat>
2025-04-01 13:37:34 +0200

    arreglo en texto de chuck

--- Assets/code/chuck/detection_Radius_Chuck.cs
+++ Assets/code/chuck/detection_Radius_Chuck.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class detection_Radius_Chuck : MonoBehaviour
 {
-    public Transform chuck;
-    public float detectionRadius = 5.0f;
+    [SerializeField] private Transform player;
+    [SerializeField] private GameObject dialeg;
+    [SerializeField] private float detectionRadius;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialeg.SetActive(false);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float distanceToPlayer = Vector2.Distance(transform.position, chuck.position);
-
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        Debug.Log(distanceToPlayer);
         if (distanceToPlayer < detectionRadius)
         {
-            //Activat texto
+            dialeg.SetActive(true);
+            dialeg.GetComponent<Dialogos_chuck>().ActivateText();
         }
     }
 
