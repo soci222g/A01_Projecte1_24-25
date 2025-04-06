f358222d4a293991fd2e64bd7bc0d02c46f1c392
m4rtiop <marti.oliveras@enti.cat>
2025-04-06 16:52:17 +0200

    top

--- Assets/code/enemis/Gobal/enemyHP.cs
+++ Assets/code/enemis/Gobal/enemyHP.cs
@@ -13,7 +13,7 @@ public class EnemyHP : MonoBehaviour
     private int HP;
 
 
-    private int dropChance = 60;
+    private int dropChance = 100;
 
     private Animator animator;
 
@@ -62,7 +62,7 @@ public class EnemyHP : MonoBehaviour
         if (randNum < dropChance)
         {
             Debug.Log("Dropping heal item.");
-            Instantiate(dropHeal, transform.position - new Vector3(0, -0.5f, 0), transform.rotation);
+            Instantiate(dropHeal, transform.position - new Vector3(0, -0.3f, 0), transform.rotation);
         }
 
         // Esperar un frame para asegurar que la animación está activa antes de obtener su duración
