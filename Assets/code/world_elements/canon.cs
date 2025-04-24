052b986497e0ed602f2659ab2e7f53e2dd3a4bea
cachau <joel.cruz@enti.cat>
2025-04-24 10:57:42 +0200

    canon anim + fix

--- Assets/code/world_elements/canon.cs
+++ Assets/code/world_elements/canon.cs
@@ -14,42 +14,24 @@ public class canon : MonoBehaviour
     }
 
     [SerializeField] GameObject proj;
-    [SerializeField] private int counter = 0;
-    [SerializeField] private int shootTime;
     [SerializeField] shootDir dir;
 
-
-    void Start()
+    void shoot()
     {
-
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        counter++;
-
-        if (counter == shootTime)
+        switch (dir)
         {
-            
-            switch(dir)
-            {
-                case shootDir.Left:
-                    Instantiate(proj, this.transform.position - new Vector3(1, 0, 0), Quaternion.Euler(0, 0, 180));
-                    break;
-                case shootDir.Right:
-                    Instantiate(proj, this.transform.position + new Vector3(1, 0, 0), Quaternion.Euler(0, 0, 0));
-                    break;
-                case shootDir.Up:
-                    Instantiate(proj, this.transform.position + new Vector3(0, 1 , 0), Quaternion.Euler(0, 0, 90));
-                    break;
-                case shootDir.Down:
-                    Instantiate(proj, this.transform.position - new Vector3(0, 1, 0), Quaternion.Euler(0, 0, 270));
-                    break;
-            }
-            
-            counter = 0;
+            case shootDir.Left:
+                Instantiate(proj, this.transform.position - new Vector3(1, 0, 0), Quaternion.Euler(0, 0, 180));
+                break;
+            case shootDir.Right:
+                Instantiate(proj, this.transform.position + new Vector3(1, 0, 0), Quaternion.Euler(0, 0, 0));
+                break;
+            case shootDir.Up:
+                Instantiate(proj, this.transform.position + new Vector3(0, 1, 0), Quaternion.Euler(0, 0, 90));
+                break;
+            case shootDir.Down:
+                Instantiate(proj, this.transform.position - new Vector3(0, 1, 0), Quaternion.Euler(0, 0, 270));
+                break;
         }
-        
     }
 }
