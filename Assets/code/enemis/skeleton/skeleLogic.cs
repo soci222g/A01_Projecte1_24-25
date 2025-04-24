44d03448215624fc7ada7a9482d5ecc636bba6e0
m4rtiop <marti.oliveras@enti.cat>
2025-04-24 16:18:44 +0200

    skeleton logic start

---
+++ Assets/code/enemis/skeleton/skeleLogic.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skeleLogic : MonoBehaviour
+{
+    public GameObject arrow;
+    [SerializeField]
+    private Animator animator;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
