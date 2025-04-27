008620054d12cc4d85387489628bf967748a02e0
m4rtiop <marti.oliveras@enti.cat>
2025-04-27 02:53:59 +0200

    Finished skeleton (minus anims)

--- Assets/code/enemis/skeleton/arrowMov.cs
+++ Assets/code/enemis/skeleton/arrowMov.cs
@@ -7,13 +7,12 @@ public class arrowMov : MonoBehaviour
     private Rigidbody2D r;
     [SerializeField]
     private float speed;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         r = GetComponent<Rigidbody2D>();
         r.AddForce(new Vector2(speed * 10000000, 0), ForceMode2D.Force);
-
     }
 
     // Update is called once per frame
@@ -31,10 +30,9 @@ public class arrowMov : MonoBehaviour
             collision.gameObject.GetComponent<hp>().setHP(1);
             Debug.Log("loose HP");
         }
-        Destroy(gameObject);
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Destroy(gameObject);
+        if (collision.gameObject.tag != "enemy")
+        {
+            Destroy(gameObject);
+        }
     }
 }
