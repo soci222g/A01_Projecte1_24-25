868f422096fe84f150126fcd674ba7578545e861
CasaTarradellas <guillem.fernandez.lopez@enti.cat>
2025-05-04 18:40:30 +0200

    a

--- Assets/code/world_elements/Door.cs
+++ Assets/code/world_elements/Door.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
 
 public class Door : MonoBehaviour
 {
