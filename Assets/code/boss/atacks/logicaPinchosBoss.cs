d3efdfb9a52342291c82617799dae6d5a0220471
CasaTarradellas <guillem.fernandez.lopez@enti.cat>
2025-05-20 20:42:42 +0200

    animaciones pinchos

--- Assets/code/boss/atacks/logicaPinchosBoss.cs
+++ Assets/code/boss/atacks/logicaPinchosBoss.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class logicaPinchosBoss : MonoBehaviour
 {
+    Animator anim;
+    int timerPinchos;
+    [SerializeField] Collider2D boxColider;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 5f);
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        anim.SetBool("pinchado", true);
         Destroy(gameObject, 0.5f);
+        if (collision.gameObject.tag == "player")
+        {
+            hp hp = collision.gameObject.GetComponent<hp>();
+            hp.setHP(1);
+        }
+    }
+
+    void startAnimOut()
+    {
+        anim.SetBool("pinchado", true);
+    }
+
+    void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+
+    void enableBoxCollider()
+    {
+        boxColider.enabled = true;
     }
 }
